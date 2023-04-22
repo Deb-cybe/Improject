208b859d02a231b1c8c1005bda87ae40aa92445e
Deb-cybe <dmjee1331@gmail.com>
2023-04-22 19:42:21 +0530

    Removed more errors.

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express=require('express');
 
 const app=express(); 
-app.use(express.json());
-app.use(urlencoded({extended:true}));
-
+app.use(express.json()); 
 app.get('/',(req,res)=>{
     res.send("It works");
 });
