8287c85d0154abd1163845be7b30e00d6714fc09
Deb-cybe <dmjee1331@gmail.com>
2023-04-22 19:17:14 +0530

    Add existing project files to Git

---
+++ app.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const https=require('https');
+
+const app=express(); 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/',(req,res)=>{
+    res.send("works");
+});
+
+app.listen(60118, console.log("Server running"));
