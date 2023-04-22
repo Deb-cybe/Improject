1ec4e99e9a5be47d2d0b270e9fefb7790d5c2bad
Deb-cybe <dmjee1331@gmail.com>
2023-04-22 20:31:29 +0530

    Installed cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express=require('express');
+const cors=require('cors');
 
 const app=express(); 
-app.use(express.json()); 
+app.use(express.json());
+app.use(cors());
+
 app.get('/',(req,res)=>{
     res.send("It works");
 });
