f5cf5ea3246e069fe869ad19a06189f677269ef8
Deb-cybe <dmjee1331@gmail.com>
2023-04-22 19:22:01 +0530

    Changed app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
-    res.send("works");
+    res.send("It works");
 });
 
 app.listen(60118, console.log("Server running"));
